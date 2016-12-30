66fc5cbfff121498e4a1e63e99b2c605ae7ef368
Admiral Potato <admiral@nuclearpixel.com>
2016-12-29 17:57:27 -0700

    if snapToCard, disable nativeScroll and handle touch inputs - prevent vertical scroll if touch vector starts horizontal - on window resize, position to leftmost full card

--- vue-swipe-cards.js
+++ vue-swipe-cards.js
@@ -14,7 +14,7 @@ Vue.component(
 				cardWidth: 0,
 				cardsVisible: 0,
 				expanderWidth: 0,
-				snapToCard: true,
+				snapToCard: false,
 				scrollInitiator: '',
 				maxScrollPosition: 0,
 				cardIndex: 0,
@@ -31,6 +31,17 @@ Vue.component(
 				type: Boolean,
 				default: false
 			}
+		},
+		created: function(){
+			let t = this;
+			//internal properties that should not be observables,
+			//and need not be redefined each time the render method is run
+			t.startX = null;
+			t.isSwiping = null;
+			t.startPoint = null;
+			t.handleTouchStart = t.createTouchHandler('Start');
+			t.handleTouchMove = t.createTouchHandler('Move');
+			t.handleTouchEnd = t.createTouchHandler('End');
 		},
 		mounted: function(){
 			let t = this;
@@ -68,6 +79,9 @@ Vue.component(
 					t.maxScrollPosition = (t.numCards -1) * t.cardWidth;
 					if(t.cardsVisible > 1){
 						t.snapToCard = false;
+					} else {
+						t.snapToCard = true;
+						t.x = t.cardIndex * t.cardWidth;
 					}
 				}
 			},
@@ -86,7 +100,7 @@ Vue.component(
 				t.animateValue({
 					valueName: 'x',
 					targetValue: cardIndex * this.cardWidth,
-					duration: 0.5,
+					duration: 0.25,
 					callback: function(){
 						t.scrollInitiator = '';
 					}
@@ -150,7 +164,7 @@ Vue.component(
 					dots
 				));
 			},
-			handleScroll(event) {
+			handleScroll: function(event) {
 				let t = this;
 				let numCards = t.numCards;
 				let x = event.currentTarget.scrollLeft;
@@ -164,17 +178,105 @@ Vue.component(
 					}
 				}
 			},
+			getPointByTouch: function(touchEvent){
+				let t = this;
+				let touches = touchEvent.changedTouches;
+				let result = null;
+				let point = null;
+				let isStart = t.startPoint === null;
+				if (isStart) {
+					result = touches[0];
+				} else {
+					for (let i = 0; i < touches.length; i++) {
+						let touch = touches[i];
+						if (touch.identifier === t.startPoint.id) {
+							result = touch;
+							break;
+						}
+					}
+				}
+				if (result) {
+					point = {
+						x: result.clientX,
+						y: result.clientY,
+						id: result.identifier
+					};
+					if(isStart){
+						t.startPoint = point;
+					}
+				}
+				return point;
+			},
+			createTouchHandler(eventName){
+				let t = this;
+				return function(touchEvent){
+					let shouldSnap = t.snapToCard && !t.scrollInitiator;
+					let point = shouldSnap ? t.getPointByTouch(touchEvent) : null;
+					if (point) {
+						t[`drag${eventName}`](point);
+					}
+				};
+			},
+			dragStart: function(point) {
+				let t = this;
+				t.startX = t.x;
+			},
+			dragMove: function(point) {
+				let t = this;
+				if (t.isSwiping === null) {
+					t.isSwiping = t.isUserScrollingHorizontally(point);
+				}
+				if (t.isSwiping) {
+					event.preventDefault();
+					t.x = t.getAbsoluteXFromRelativeX(point.x);
+				}
+			},
+			dragEnd: function(point){
+				let t = this;
+				if (t.isSwiping) {
+					t.x = t.getAbsoluteXFromRelativeX(point.x);
+					let scrollDirection = this.getRelativeDragDirection(point.x);
+					let nextCardIndex = Math.min(
+						t.numCards - 1,
+						Math.max(0, t.cardIndex - scrollDirection)
+					);
+					t.setIndex(nextCardIndex);
+				}
+				t.startX = null;
+				t.startPoint = null;
+				t.isSwiping = null;
+			},
+			getRelativeDragDirection: function(x){
+				let t = this;
+				let xDiff = x - t.startPoint.x;
+				let sign = (xDiff / Math.abs(xDiff));
+				return isNaN(sign) ? 0 : sign;
+			},
+			getAbsoluteXFromRelativeX: function(x){
+				return this.startX + (this.startPoint.x - x);
+			},
+			isUserScrollingHorizontally: function(point) {
+				let x = Math.abs(point.x - this.startPoint.x);
+				let y = Math.abs(point.y - this.startPoint.y);
+				return x > y;
+			}
 		},
 		computed: {
 			styles: function(){
+				let t = this;
 				return `
-					#${this.id} .swipe-card{
-						width: ${this.cardWidth}px;
+					#${t.id} .swipe-card{
+						width: ${t.cardWidth}px;
 					}
-					#${this.id} .swipe-expander{
-						width: ${this.expanderWidth}px;
+					#${this.id}.vue-swipe-cards .swipe-scroller .swipe-expander{
+						width: ${t.expanderWidth}px;
 					}
 				`;
+			},
+			translateStyle: function () {
+				let t = this;
+				let translate = `translate3d(${-t.x}px, 0, 0)`;
+				return `-webkit-transform: ${translate};transform: ${translate};`;
 			}
 		},
 		render: function (createElement) {
@@ -193,37 +295,29 @@ Vue.component(
 				undefined,
 				t.styles
 			);
+			let swipeExpanderOptions = {
+				staticClass: 'swipe-expander'
+			};
+			if(t.snapToCard){
+				swipeExpanderOptions.style = t.translateStyle;
+			}
 			let swipeExpander = createElement(
 				'div',
-				{
-					staticClass: 'swipe-expander'
-				},
+				swipeExpanderOptions,
 				slots
 			);
-			let domProps = {};
-			if(t.snapToCard){
-				domProps.scrollLeft = t.x;
-			}
 			let swipeScroller = createElement(
 				'div',
 				{
 					staticClass: 'swipe-scroller',
 					'class': {
+						nativeScroll: !t.snapToCard,
 						momentumScroll: !t.snapToCard
 					},
-					domProps: domProps,
 					on:{
-						touchstart: function(event) {
-							if(t.scrollInitiator){
-								event.preventDefault();
-							}
-						},
-						touchend: function(event){
-							if(t.snapToCard){
-								event.preventDefault();
-								t.setIndex(t.cardIndex);
-							}
-						},
+						touchstart: t.handleTouchStart,
+						touchmove: t.handleTouchMove,
+						touchend: t.handleTouchEnd,
 						scroll: function(event){
 							if(!t.scrollInitiator){
 								t.handleScroll(event);
